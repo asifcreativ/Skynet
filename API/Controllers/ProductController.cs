6afe0ebefa519396360ae1bd66f1df72767171da
Muhammad Asif <muhammadasif@Muhammads-MBP.attlocal.net>
2020-05-24 12:00:51 -0700

    add entities Brands and Types; add seed data; implemented APIs for get Brands and Types

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -13,25 +14,37 @@ namespace API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly ILogger<ProductsController> _logger;
-        private readonly StoreContext _context;
+        private readonly IProductRepository _productRepository;
 
-        public ProductsController(ILogger<ProductsController> logger, StoreContext context)
+        public ProductsController(ILogger<ProductsController> logger, IProductRepository productRepository)
         {
-            _context = context;
+            _productRepository = productRepository;
             _logger = logger;
         }
 
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            return Ok(await _productRepository.GetProductsAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _productRepository.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productRepository.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _productRepository.GetProductTypesAsync());
         }
     }
 }
